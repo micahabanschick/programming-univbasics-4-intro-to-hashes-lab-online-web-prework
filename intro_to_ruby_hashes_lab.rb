854f7b69a417ebe46e0e9049d89410e5a229578e
Micah Banschick <micha.ban@gmail.com>
2020-01-15 23:26:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,10 +5,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hash = {this: "is",a: "hash"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {name: "Grace Hopper"}
 end
 
 def id_generator
